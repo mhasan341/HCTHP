46bbbbbb1157978a9cde73328f558e8751e5ee1b
Mahmudul Hasan <mhasan341@gmail.com>
2024-07-12 22:20:37 +0600

    Initial Commit

---
+++ HCTHP/HCTHPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HCTHPApp.swift
+//  HCTHP
+//
+//  Created by Mahmudul Hasan on 2024-07-12.
+//
+
+import SwiftUI
+
+@main
+struct HCTHPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
