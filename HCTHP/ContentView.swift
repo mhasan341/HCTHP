490b34f0544d125f034932bf486ceac93d30a089
Mahmudul Hasan <mhasan341@gmail.com>
2024-07-17 09:33:22 +0600

    Update Navigation Navigation updated with few changes in registrationView for better user experience

---
+++ HCTHP/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HCTHP
+//
+//  Created by Mahmudul Hasan on 2024-07-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HCTHP/Views/Medication/MedicationHome.swift
@@ -0,0 +1,18 @@
+//
+//  MedicationHome.swift
+//  HCTHP
+//
+//  Created by Mahmudul Hasan on 2024-07-17.
+//
+
+import SwiftUI
+
+struct MedicationHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MedicationHome()
+}
